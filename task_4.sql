227421f573e409114712516f9cb5af1a3520e0bf
Vbrav01 <victortosh001@gmail.com>
2024-10-19 13:29:54 +0300

    alx_book_store.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,12 @@
-USE alx_book_store;
-SELECT * FROM Books; 
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Column Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    information_schema.columns
+WHERE 
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'Books';
