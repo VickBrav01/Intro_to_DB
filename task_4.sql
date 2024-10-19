d62c49e7d2ba523900bff8ae30df8518b58902db
Vbrav01 <victortosh001@gmail.com>
2024-10-19 13:34:44 +0300

    alx_book_store.sql

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,1 @@
-SELECT 
-    COLUMN_NAME AS 'Column Name',
-    COLUMN_TYPE AS 'Column Type',
-    IS_NULLABLE AS 'Is Nullable',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM 
-    INFORMATION_SCHEMA.COLUMN
-WHERE 
-    TABLE_SCHEMA = "alx_book_store"
-    AND TABLE_NAME = 'Books';
+SHOW CREATE TABLE Books;
