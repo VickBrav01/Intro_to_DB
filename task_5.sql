dc5655a31e5027cf0c541d3b101c6bd3902583d0
Vbrav01 <victortosh001@gmail.com>
2024-10-19 14:57:45 +0300

    alx_book_store.sql

---
+++ task_5.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE IF NOT EXISTS Customers (
+--     customer_id INT PRIMARY KEY,
+--     customer_name VARCHAR(215),
+--     email VARCHAR(215),
+--     address TEXT
+-- );
+USE  alx_book_store;
+
+INSERT INTO Customers(customer_id, customer_name, email, address)
+VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
