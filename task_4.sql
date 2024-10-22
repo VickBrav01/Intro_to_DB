4616ca6be275f9eb77dd334fffba5c76529c91f3
Vbrav01 <victortosh001@gmail.com>
2024-10-22 11:00:11 +0300

    Arithmetic operations

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,12 @@
-SHOW TABLE STATUS LIKE 'Books';
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Column Type',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' -- This ensures it runs for the currently selected database
+    AND TABLE_NAME = 'Books';
